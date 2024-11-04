1cf0d50139c4a4725a8a3229f926e268d82b3ab7
bobbythree <mynameisbobbylewis@hotmail.com>
2024-11-04 16:28:26 -0600

    add subseries button and func

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   const titlesBtn = document.getElementById('titles-btn');
   const pageOutput = document.getElementById('page-output');
+  const subBtn = document.getElementById('sub-btn');
 
   async function getTitles() {
     const url = '/titles';
@@ -11,4 +12,14 @@ window.onload = () => {
   }
 
   titlesBtn.onclick = () => getTitles();
+
+  async function getSubseries() {
+    const url = '/subseries';
+    const res = await fetch(url);
+    const data = await res.json();
+    pageOutput.innerText = JSON.stringify(data, null, 2)
+      // console.log(data)
+    }
+  
+    subBtn.onclick = () => getSubseries();
 }
