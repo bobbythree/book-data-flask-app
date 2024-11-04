cfa003c7ea430463ed5b2d3c659cdfee667551b9
bobbythree <mynameisbobbylewis@hotmail.com>
2024-11-04 17:54:18 -0600

    code cleanup

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+This is the main py file and entry point to the program. It connencts to the
+mySQL database, contains url routes and mySQL queries.  
+"""
+
+
 from flask import Flask, jsonify, render_template
 import mysql.connector
 from config import config
@@ -12,23 +18,28 @@ cursor = db.cursor()
 
 @app.route("/")
 def index():
+    """send html to the home page"""
+
     return render_template('index.html')
+
 
 @app.route("/titles")
 def titles():
+    """queries the db and returns book titles as json."""
+
     cursor.execute("SELECT title FROM novels")
     result = cursor.fetchall()
     output = []
     for x in result:
         x = x[0]
-        output.append(x) 
-    return jsonify(output)        
-        
-    db.close()
-    cursor.close()  
+        output.append(x)
+    return jsonify(output)
+
 
 @app.route("/subseries")
 def titles_and_subseries():
+    """queries the db and returns book titles and subseries as json."""
+
     cursor.execute(
         """SELECT novels.title, subseries.subseries_name FROM novels
         INNER JOIN subseries ON novels.subseries_id = subseries.subseries_id"""
